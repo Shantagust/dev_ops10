b062406bb09f6499b6911ff56f9fdb38b3be6694
Molchanov Ihor <imolchanov@kdmg.com.ua>
2024-11-23 11:39:50 +0200

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,11 @@
 provider "aws" {
   region = "eu-central-1"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
 
 resource "aws_instance" "web_server" {
-  ami           = "ami-0daf65a18bb5ab7a8" # Replace with your AMI ID
+  ami           = "ami-0daf65a18bb5ab7a8"
   instance_type = "t2.micro"
   user_data = <<-EOF
               #!/bin/bash
